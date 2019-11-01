c45124fa73f8083129cf6ee7c7c4e4ddfe056f41
akram256 <mukasaakram55@gmail.com>
2019-11-01 14:56:04 +0300

    add react to app component

--- react/src/App.js
+++ react/src/App.js
@@ -4,21 +4,38 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <form id="msform">
+            <ul id="progressbar">
+              <li class="active">Personal Details</li>
+              <li>Question one</li>
+              <li>Question two</li>
+              <li>Question three</li>
+              <li>Question four</li>
+              <li>Question five</li>
+              <li>Question six</li>
+              <li>Results</li>
+            </ul>
+
+            <div>
+              <fieldset>
+                <h2 class="fs-title">Personal Details</h2>
+                <h3 class="fs-subtitle">Tell us something more about you</h3>
+                <input type="text" name="fname" placeholder="First Name" />
+                <input type="text" name="lname" placeholder="Last Name" />
+                <input type="text" name="phone" placeholder="Phone" />
+                <input type="email" name="email" placeholder="Email" />
+                <input type="text" name="address" placeholder="Address" />
+                <input type="text" name="nationality" placeholder="Nationality" />
+                <input type="button" name="next" class="next action-button" value="Next" />
+              </fieldset>
+            </div>
+          </form>
+        </div>
+
+      </div>
     </div>
   );
 }
